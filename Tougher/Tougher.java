accbaced703be27185271b39f12716102e10f9a3
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-10-13 11:34:06 +0530

    Create Tougher.java

---
+++ Tougher/Tougher.java
@@ -0,0 +1,7 @@
+public class Tougher{
+   public static void main(String[] args){
+       
+      System.out.println("Working for Honeywell");
+      System.out.println("Expecting Offer from Moody's Analytics");
+   }
+}
