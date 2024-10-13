81c5b05f6db384276cf2b5021015d859f5755c82
Amit Singh <47208399+aamitsinghh10@users.noreply.github.com>
2024-10-13 11:35:33 +0530

    Delete Tougher directory

--- Tougher/Tougher.java
+++
@@ -1,7 +0,0 @@
-public class Tougher{
-   public static void main(String[] args){
-       
-      System.out.println("Working for Honeywell");
-      System.out.println("Expecting Offer from Moody's Analytics");
-   }
-}
